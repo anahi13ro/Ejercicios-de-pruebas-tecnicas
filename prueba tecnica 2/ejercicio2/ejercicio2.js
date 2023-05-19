2f20ab864967d58f87286c10477de52490cfb650
anahi13ro <roandrada510@gmail.com>
2023-05-18 22:04:15 -0300

    pruebasTecnicas2

---
+++ prueba tecnica 2/ejercicio2/ejercicio2.js
@@ -0,0 +1,7 @@
+let number = prompt('Ingrese un número :');
+
+if(number % 5 == 0) {
+    console.log("El número es divisible en 5!!")
+} else{
+    console.log("El número no es divisible en 5")
+}
---
+++ prueba tecnica 2/ejercicio4/ejercicio4.js
@@ -0,0 +1,11 @@
+let num1 = prompt('Ingrese el número 1');
+let num2 = prompt('Ingrese el número 2');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+if (num1 > num2) {
+     alert('El número 1 es mayor!!');
+} else {
+     alert('El número 2 es el mayor!!')
+}
